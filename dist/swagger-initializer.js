1da5e5932317831110f74bd78f0bdc61a8256b72
Daniel Cugler <danielcugler@gmail.com>
2025-08-17 15:53:27 -0300

    end-no-edit

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: window.yamlToLoad,
+    url: "cifra-cesar-api-spec.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
